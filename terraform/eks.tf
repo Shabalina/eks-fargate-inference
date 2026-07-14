346d3604ae7f9c057240385a06e24cfba1a4dc3a
Anastasia Shapovalova <shabalinastya@Romans-MacBook-Air.local>
2026-07-14 15:26:13 +0100

    fix resource name

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -112,7 +112,7 @@ data "tls_certificate" "eks" {
 }
 
 # Register the OIDC Provider with AWS IAM
-resource "aws_iam_oidc_provider" "main" {
+resource "aws_iam_openid_connect_provider" "main" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
   url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
@@ -131,7 +131,7 @@ data "aws_iam_policy_document" "lbc_assume_role" {
     }
 
     principals {
-      identifiers = [aws_iam_oidc_provider.main.arn] 
+      identifiers = [aws_iam_openid_connect_provider.main.arn] 
       type        = "Federated"
     }
   }
